8c81815f7da1d5b6801667dce37aaa54d9a6d250
Long Tran <longtran13112004@gmail.com>
2025-09-18 20:24:28 +0700

    add 2 file test.html and test.js

---
+++ ss6_JavaScript/test.js
@@ -0,0 +1,19 @@
+//bai 1
+var i = 10;
+var f = 20.5;
+var b =true;
+var s = "Ha Noi";
+
+let width = 10;
+let height = 15;
+let area = width * height;
+alert("Area = " + area)
+
+var a = prompt("Enter a number");
+var d = prompt("Enter b number");
+  if(a%d===0) {
+    alert("A a boi so cua b")
+}
+  else{
+    alert("A khong phai la boi so cua b")
+  }
